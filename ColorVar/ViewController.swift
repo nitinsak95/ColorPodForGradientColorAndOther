3d1c8fc667d7fff9ba11b78aceeab98fe6773781
affixusimac1 <viveking@outlook.com>
2018-10-10 13:08:22 +0530

    sddddd

--- ColorVar/ViewController.swift
+++ ColorVar/ViewController.swift
@@ -7,12 +7,37 @@
 //
 
 import UIKit
+import ChameleonFramework
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var newView: UIView!
+    @IBOutlet weak var view1: UIView!
+    @IBOutlet weak var view2: UIView!
+    @IBOutlet weak var view3: UIView!
+    @IBOutlet weak var view4: UIView!
+    @IBOutlet weak var view5: UIView!
+    @IBOutlet weak var view6: UIView!
+    @IBOutlet weak var view7: UIView!
+    
+    let firstRandomColor = UIColor.white
+    let secondRandomColor = UIColor.lightGray
+    let thirdRandomColor = UIColor.randomFlat
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view1.backgroundColor = GradientColor(gradientStyle: .topToBottom, frame: view.frame, colors: [firstRandomColor, secondRandomColor])
+        view2.backgroundColor = GradientColor(gradientStyle: .leftToRight, frame: view.frame, colors: [firstRandomColor, secondRandomColor])
+          view3.backgroundColor = GradientColor(gradientStyle: .radial, frame: view.frame, colors: [firstRandomColor, secondRandomColor])
+         newView.backgroundColor = GradientColor(gradientStyle: .topToBottom, frame: view.frame, colors: [firstRandomColor, secondRandomColor])
+
+        let colors:[UIColor] = [
+            UIColor.flatPurpleColorDark(),
+            UIColor.flatWhite()
+        ]
+        newView.backgroundColor = GradientColor(gradientStyle: .topToBottom, frame: view.frame, colors: colors)
+        
     }
 
     override func didReceiveMemoryWarning() {
